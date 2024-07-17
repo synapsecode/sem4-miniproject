4870fe8807e6f337848009e307d8af91f278ec0b
Manas Hejmadi <manas2all@outlook.com>
2024-07-18 04:02:35 +0530

    Initial Commit

---
+++ attendance.py
@@ -0,0 +1,59 @@
+import cv2
+import face_recognition
+import os
+import numpy as np
+from datetime import datetime
+import requests
+from flask import jsonify
+
+training_images_dir = "Training_images"
+known_face_encodings = []
+known_face_names = []
+
+def perform_training():
+    # Load training images and encode faces
+    for filename in os.listdir(training_images_dir):
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            image_path = os.path.join(os.getcwd(),training_images_dir, filename)
+
+            image = face_recognition.load_image_file(image_path)
+            encodings = face_recognition.face_encodings(image)
+            if encodings:
+                known_face_encodings.append(encodings[0])
+                known_face_names.append(os.path.splitext(filename)[0])
+    print('Training Complete!')   
+
+def perform_inference(image):
+    try:
+        image_np = np.frombuffer(image, np.uint8)
+        group_image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        rgb_group_image = cv2.cvtColor(group_image, cv2.COLOR_BGR2RGB)
+        face_locations = face_recognition.face_locations(rgb_group_image)
+        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        attendance_data = []
+        for i, (top, right, bottom, left) in enumerate(face_locations):
+            face_encoding = face_recognition.face_encodings(
+                rgb_group_image, [face_locations[i]])[0]
+
+            matches = face_recognition.compare_faces(
+                known_face_encodings, face_encoding, tolerance=0.6
+            )
+            face_distances = face_recognition.face_distance(
+                known_face_encodings, face_encoding
+            )
+
+            best_match_index = None
+            if True in matches:
+                best_match_index = face_distances.argmin()
+
+            if best_match_index is not None and matches[best_match_index]:
+                name = known_face_names[best_match_index]
+            else:
+                name = "Unknown"
+
+            name = name + "@dsce.edu.in"
+
+            attendance_data.append({"Name": name})
+        return jsonify(attendance_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
---
+++ run.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request, render_template,send_from_directory
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+from attendance import perform_inference, perform_training
+import requests
+import os
+
+app = Flask(__name__)
+CORS(app)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+def get_attendance(image_link):
+    perform_training()
+    image = requests.get(image_link)
+    return perform_inference(image.content)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            fp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(fp)
+            filepath = f"http://localhost:3000/getimage/{filename}"
+            return get_attendance(filepath)
+    return "UPLOAD"
+
+@app.route('/getimage/<fn>')
+def getimage(fn):
+    return send_from_directory('static/uploads', fn)
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=3000, debug=True)
